8fc566aa3e47163a74f48564a3309fcf4893d07b
SourishBiswas23 <sourish135@gmail.com>
2024-07-06 22:43:03 +0530

    Added keybinding for finding commands in telescope

--- lua/sourish/plugins/telescope.lua
+++ lua/sourish/plugins/telescope.lua
@@ -33,7 +33,7 @@ return {
 		keymap.set("n", "<leader>fh", "<cmd>Telescope find_files hidden=true<cr>", { desc = "Fuzzy find hidden files" })
 		keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find old files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>fc", "<cmd>Telescope commands<cr>", { desc = "Find commands" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 		keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find todos" })
 	end,
