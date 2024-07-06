06c1bf5a906958756de5d73158c6e4fd94bea3cc
SourishBiswas23 <sourish135@gmail.com>
2024-07-06 18:33:31 +0530

    added a gitignore and keybinds for trouble

--- lua/sourish/plugins/trouble.lua
+++ lua/sourish/plugins/trouble.lua
@@ -1,12 +1,40 @@
 return {
-  "folke/trouble.nvim",
-  dependencies = {"DaikyXendo/nvim-material-icon","folke/todo-comments.nvim" },
-  keys = {
-    -- { "<leader>tt", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
-    -- { "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-    -- { "<leader>td", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-    -- { "<leader>tq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-    -- { "<leader>tl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
-    -- { "<leader>td", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
-  },
+	"folke/trouble.nvim",
+	dependencies = { "DaikyXendo/nvim-material-icon", "folke/todo-comments.nvim" },
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>td",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>tb",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>ts",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>tl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>to",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+
+		{
+			"<leader>tq",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+		{ "<leader>tm", "<cmd>Trouble<cr>", desc = "Trouble menu" },
+	},
 }
