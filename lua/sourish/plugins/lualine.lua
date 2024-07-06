836eb3a252f95b32adcc42d68574efd6609601d2
SourishBiswas23 <sourish135@gmail.com>
2024-07-06 16:28:39 +0530

    fixed the lualine
    Made it so that there is only one lualine, changed the theme and modules
    in the lualine

--- lua/sourish/plugins/lualine.lua
+++ lua/sourish/plugins/lualine.lua
@@ -3,58 +3,26 @@ return {
 	dependencies = { "DaikyXendo/nvim-material-icon" },
 	config = function()
 		local lualine = require("lualine")
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-
-		local colors = {
-			blue = "#65D1FF",
-			green = "#3EFFDC",
-			violet = "#FF61EF",
-			yellow = "#FFDA7B",
-			red = "#FF4A4A",
-			fg = "#c3ccdc",
-			bg = "#112638",
-			inactive_bg = "#2c3043",
-		}
-
-		local my_lualine_theme = {
-			normal = {
-				a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			insert = {
-				a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			visual = {
-				a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			command = {
-				a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			replace = {
-				a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			inactive = {
-				a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-				b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-				c = { bg = colors.inactive_bg, fg = colors.semilightgray },
-			},
-		}
-
-		-- configure lualine with modified theme
+		local lazy_status = require("lazy.status")
 		lualine.setup({
 			options = {
-				theme = my_lualine_theme,
+				theme = "nightfly",
+				globalstatus = true,
+				component_separators = { left = "", right = "" },
 			},
 			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {
+					"branch",
+					{
+						"diff",
+						cond = function()
+							return vim.b.gitsigns_head ~= nil
+						end,
+					},
+					"diagnostics",
+				},
+				lualine_c = { "searchcount" },
 				lualine_x = {
 					{
 						lazy_status.updates,
@@ -65,6 +33,8 @@ return {
 					{ "fileformat" },
 					{ "filetype" },
 				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
 			},
 		})
 	end,
