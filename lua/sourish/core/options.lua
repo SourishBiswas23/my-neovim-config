751fcd82cecb1993e7764737a1aaafa853abb7a7
SourishBiswas23 <sourish135@gmail.com>
2024-07-07 12:01:05 +0530

    Can now open help in vertical split

--- lua/sourish/core/options.lua
+++ lua/sourish/core/options.lua
@@ -69,3 +69,14 @@ api.nvim_create_autocmd("BufWinEnter", {
 	desc = "Load view (folds) when opening file",
 	command = "silent! loadview",
 })
+
+-- Open help window in a vertical split to the right.
+api.nvim_create_autocmd("BufWinEnter", {
+	group = vim.api.nvim_create_augroup("help_window_right", {}),
+	pattern = { "*.txt" },
+	callback = function()
+		if vim.o.filetype == "help" then
+			vim.cmd.wincmd("L")
+		end
+	end,
+})
