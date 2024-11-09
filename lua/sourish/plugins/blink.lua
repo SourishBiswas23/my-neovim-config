ef6b613a5fe9367a1eb634b030e4b50dc1894308
SourishBiswas23 <sourish135@gmail.com>
2024-11-09 19:00:51 +0530

    Added option for hiding message in blink

--- lua/sourish/plugins/blink.lua
+++ lua/sourish/plugins/blink.lua
@@ -28,6 +28,8 @@ return {
 			},
 		},
 		keymap = {
+			preset = "default",
+			["<C-e>"] = { "hide", "fallback" },
 			["<CR>"] = { "accept", "fallback" },
 			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
 			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
